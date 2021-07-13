2c9d9ef33de5ef8b0eaf3591662d1b7322d083c5
DianaDanielyan <86835352+DianaDanielyan@users.noreply.github.com>
2021-07-13 17:02:53 +0400

    Add files via upload

---
+++ mushrooms.js
@@ -0,0 +1,15 @@
+class Mushrooms extends LivingCreature {
+    constructor(x, y) {
+        super(x,y)
+        this.energy = 5;
+    }
+
+    chooseCell(ch) {
+        this.getNewCoordinates()
+        return super.chooseCell(ch)
+    }
+
+
+
+
+}
